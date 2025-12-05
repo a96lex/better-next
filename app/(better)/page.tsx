e8ceba9b5549c8b729bee5796df49a553144d663
a96lex <a96lex@gmail.com>
2025-12-05 16:12:38 +0100

    feat: tw and some changes!

--- app/(better)/page.tsx
+++ app/(better)/page.tsx
@@ -3,6 +3,7 @@
 import { signOut } from "next-auth/react";
 import { api } from "@/app/lib/trpc/client";
 import { useState } from "react";
+import { RotateCcw } from "lucide-react";
 
 export default function Home() {
   const [newGoalName, setNewGoalName] = useState("");
@@ -33,15 +34,28 @@ export default function Home() {
   };
 
   return (
-    <div className="min-h-screen p-8">
+    <div className="min-h-screen p-4">
       <button onClick={() => signOut()}>signout</button>
-      {goals?.map((g) => {
-        return (
-          <div key={g.id}>
-            {g.name} - {g.description}
-          </div>
-        );
-      })}
+      <h1>Tus contadores</h1>
+      <div className="grid grid-cols-2 gap-4">
+        {goals?.map((g, idx) => {
+          return (
+            <div key={g.id}>
+              <div
+                className={`bg-goal-${idx % 6} flex items-end justify-between rounded-xl p-2 text-goal-foreground-${idx % 6} h-28`}
+              >
+                <div>
+                  <h1>{String(g.events?.[0]?.timestamp ?? "-")}</h1>
+                  <p>días</p>
+                </div>
+                <RotateCcw className="bg-foreground/10 h-8 w-14 rounded-full p-1" />
+              </div>
+              <h2 className="mt-1 pl-1">{g.name}</h2>
+              <h2 className="mt-1 pl-1">{g.description}</h2>
+            </div>
+          );
+        })}
+      </div>
 
       <input
         value={newGoalName}
