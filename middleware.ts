7fa54b964a8bcd0033f565f2c9f620868658ed19
a96lex <a96lex@gmail.com>
2025-12-09 00:02:55 +0100

    chore: match the root path for redirecting toauth

--- middleware.ts
+++ middleware.ts
@@ -6,5 +6,8 @@ const { auth } = NextAuth(authConfig);
 export default auth;
 
 export const config = {
-  matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico|.*\\.map).*)"],
+  matcher: [
+    "/",
+    "/((?!api/auth|_next/static|_next/image|favicon.ico|.*\\.map).*)",
+  ],
 };
