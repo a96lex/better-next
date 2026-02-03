f434dbb1a940d12fb658356f30f63a52ddbad5af
a96lex <a96lex@gmail.com>
2026-02-03 17:43:40 +0100

    feat: add loading state

--- app/(better)/page.tsx
+++ app/(better)/page.tsx
@@ -5,25 +5,32 @@ import { api } from "@/app/lib/trpc/client";
 import { useTranslations } from "next-intl";
 import CreateGoal from "./components/create-goal";
 import GoalItem from "./components/goal-view";
+import { LoaderCircle } from "lucide-react";
 
 export default function Home() {
   const t = useTranslations("goal.list");
-
-  const { data: goals } = api.goal.getAll.useQuery();
+  const { data: goals, isLoading } = api.goal.getAll.useQuery();
 
   return (
     <div className="min-h-screen p-4">
       <button onClick={() => signOut()}>signout</button>
+
       <h1>{t("header")}</h1>
-      <div className="grid grid-cols-1 gap-4">
-        {goals?.length ? (
-          goals.map((g, idx) => {
-            return <GoalItem key={g.id} idx={idx} goal={g} />;
-          })
-        ) : (
-          <div>{t("empty")}</div>
-        )}
-      </div>
+
+      {isLoading ? (
+        <div className="fixed inset-0 flex items-center justify-center">
+          <LoaderCircle className="animate-spin" />
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 gap-4">
+          {goals?.length ? (
+            goals.map((g, idx) => <GoalItem key={g.id} idx={idx} goal={g} />)
+          ) : (
+            <div>{t("empty")}</div>
+          )}
+        </div>
+      )}
+
       <CreateGoal />
     </div>
   );
